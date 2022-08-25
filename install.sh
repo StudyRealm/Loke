80441cc75a9e75f60fd8c7fd5fa82529be6656e2
StudyRealm <110121785+StudyRealm@users.noreply.github.com>
2022-08-26 00:17:40 +0530

    Add files via upload

--- install.sh
+++ install.sh
@@ -11,39 +11,39 @@
 
 lsblk 
 
-:'
-		Disk partitioning 		
-'
+#		DISK PARTITION
+
 
 read -p "Enter dev: " device
 
 #(echo p; echo g;echo n;echo 1;echo "";echo "+300M";echo n;echo 2;echo "";echo "";echo t;echo 1:echo 1:echo w) | fdisk /dev/$device 
 
-cat ./cmd_dialoug  | fdisk /dev/$device 
+cat ./fdisk_cmd_dialouge  | fdisk /dev/$device 
 
-
-
-:'
-		Formating partitions with required filesystem
-'
+#		FORMAT THE DISKS
 
 mkfs.fat -F 32 /dev/$device"1"
 
 mkfs.ext4 /dev/$device"2"
 
-
-
-:'
-		Mount partitions 
-'
+#		MOUNT THE PARTITIONS
 
 mount --mkdir /dev/$device"1" /mnt/boot
 
 mount /dev/$device"2" /mnt/ 
 
-
 #need to make and format custom number of partitions in GPT, use arrays
 #create options to make stacked block devices for LVM, system_encrypt and RAID.
+
+# 		INSTALLATION 
+
+:'
+		choosing 'n' numbers of https mirrors sort by download rate
+'
+
+read -p "Enter number of mirrors: " nums
+
+reflector --latest nums --protocol https --sort rate --save /etc/pacman.d/mirrorlist 
 
 :'
 		Installing base pkgs, linux kernel/specific kernel pkg, linux
@@ -53,4 +53,10 @@ mount /dev/$device"2" /mnt/
 			2.pacman, req=chroot into system.
 '
 
-pacstrap 
+pacstrap /mnt base linux linux-firmware
+
+#		CONFIGURE THE SYSTEM
+
+genfstab -U /mnt >> /mnt/etc/fstab
+
+cat ./chroot_cmd | arch-chroot /mnt
